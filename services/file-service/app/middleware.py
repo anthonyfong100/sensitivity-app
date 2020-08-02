c8be58e649dbf488e037142cbcd8bf2f6dfb8912
Anthony <anthonyfong100@gmail.com>
2020-08-02 20:50:07 +0800

    Add middleware to verify tokens

---
+++ services/file-service/app/middleware.py
@@ -0,0 +1,30 @@
+import os
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
+from jose import JWTError, jwt
+
+auth = HTTPBearer()
+
+
+async def verify_token(
+    jwt_token: HTTPAuthorizationCredentials = Depends(auth),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        print(jwt_token.credentials)
+        payload = jwt.decode(
+            jwt_token.credentials,
+            os.getenv("SECRET_KEY"),
+            algorithms=["HS256"],
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except (JWTError, KeyError):
+        raise credentials_exception
--- services/file-service/main.py
+++ services/file-service/main.py
@@ -1,15 +1,19 @@
 import os
 
 import uvicorn
-from app import (
-    config,
-)  # this line needs to be the first few as it imports the .env into sys variable
+from app import config
+from app.middleware import verify_token
 from app.routes import file_router
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
-app = FastAPI()
+app = FastAPI(docs_url="/docs", redoc_url=None)
 
-app.include_router(file_router, prefix="/api/file", tags=["file"])
+app.include_router(
+    file_router,
+    prefix="/api/file",
+    tags=["file"],
+    dependencies=[Depends(verify_token)],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=7000)
