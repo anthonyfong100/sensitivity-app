3b2975f2151adffbba81e242215b7159772a429b
Anthony <anthonyfong100@gmail.com>
2020-08-02 21:48:47 +0800

    Add unique constrain for file database

--- services/file-service/app/db/models.py
+++ services/file-service/app/db/models.py
@@ -1,11 +1,14 @@
 from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy.schema import UniqueConstraint
 
 from .database import Base
 
 
 class File(Base):
     __tablename__ = "files"
-
+    __table_args__ = (
+        UniqueConstraint("username", "filename", name="_username_filename_uc"),
+    )
     id = Column(Integer, primary_key=True)
     username = Column(String, index=True)
     filename = Column(String, index=True)
